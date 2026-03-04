ba83f3a6b93e483d8264ff7ae0e6409d9a3a9a99
ydah <t.yudai92@gmail.com>
2026-03-04 17:28:32 +0900

    Initial commit

---
+++ lib/clef.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clef/version"
+
+module Clef
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/clef/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Clef
+  VERSION = "0.1.0"
+end
